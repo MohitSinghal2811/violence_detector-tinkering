be595fb9105dc6e9eda0ac2c1bf71ce893386be7
Mohit <2018eeb1165@iitrpr.ac.in>
2020-04-30 13:06:04 +0530

    1000 images downloaded
    Signed-off-by: Mohit <2018eeb1165@iitrpr.ac.in>

--- python scripts/scrape.py
+++ python scripts/scrape.py
@@ -2,18 +2,43 @@ import requests
 import cv2
 import os
 import csv
-import base64
+import sys
+             
+pathname = os.path.abspath(sys.argv[0])         
+base_path = os.path.abspath(os.path.dirname(os.path.dirname(pathname)))
 
-prop_file = open('1.csv', 'r')
+prop_file = open(os.path.join(base_path,'csvs\\kritik.csv'), 'r')
 csv_reader = csv.reader(prop_file)
 counter = 0
 for row in csv_reader:
     try:
             coded_url = row[0]
-            image_url = base64.b64decode(coded_url)
-            r = requests.get(image_url)
-            with open(str(counter) + ".png",'wb') as f: 
+            r = requests.get(coded_url)
+            with open(os.path.join(base_path,"data\\training_images\\non-violent\\" + str(counter) + ".png"),'wb') as f: 
                 f.write(r.content)
+            print( str(counter) + " Non-violent Image(s) Downloaded ", end = "\r")
             counter = counter + 1
     except Exception as e:
             print(e)
+            print("Failed to download url number :-", end = " ")
+            print(counter)
+
+print()
+
+counter = 0
+prop_file = open(os.path.join(base_path,'csvs\\tanya.csv'), 'r')
+csv_reader = csv.reader(prop_file)        
+for row in csv_reader:
+    try:
+            coded_url = row[0]
+            r = requests.get(coded_url)
+            with open(os.path.join(base_path,"data\\training_images\\violent\\" + str(counter) + ".png"),'wb') as f: 
+                f.write(r.content)
+            print( str(counter) + " violent Image(s) Downloaded ", end = "\r")
+            counter = counter + 1
+    except Exception as e:
+            print(e)
+            print("Failed to download url number :-", end = " ")
+            print(counter)
+
+print()
